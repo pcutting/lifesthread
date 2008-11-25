a0390883add8bfaa5496359ad78b846fe3886a2a
Phil <phil@phil-laptop.(none)>
2008-11-25 13:54:03 -0500

    cleaned up reports admin and member

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -2,23 +2,28 @@ module ReportsHelper
 
 def minitable(table_header, table_labels, table_data)
 
-html_block = "#{table_header}<br /><table class='mini'><tr>" #string for returning html block.
+  html_block = "#{table_header}<br /><table class='mini'><tr>" #string for returning html block.
 
-table_labels.each do |label|
-  html_block += "<th>#{label}</th>"
+  table_labels.each do |label|
+    html_block += "<th>#{label}</th>"
+  end
+
+  html_block += "</tr>"
+
+  table_data.each do |data|
+    html_block += "<tr><td>#{data.join('</td><td>')}</tr>\n"
+  end
+
+
+
+  html_block += "</table>"  #return code for table.
 end
 
-html_block += "</tr>"
+def setup
 
-table_data.each do |data|
 
-html_block += "<tr><td>#{data.join('</td><td>')}</tr>\n"
 
 end
 
-
-
-html_block += "</table>"  #return code for table.
-end
 
 end
