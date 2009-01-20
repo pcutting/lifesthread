4b833b522923702e8ab73073d77ccf8d74a1a4ab
Phil <phil@phil-laptop.(none)>
2009-01-20 18:29:44 -0500

    member role granting cleaned up

--- app/controllers/member/users_controller.rb
+++ app/controllers/member/users_controller.rb
@@ -4,8 +4,6 @@ before_filter :load_member_conditions
 
   def index
     @users = User.paginate :page => params[:page] , :per_page => 100 , :order => 'login asc' ,  :include => :profile, :conditions => ["profiles.zip IN (?)", @member_conditions ] 
-
-
   end
 
   def show
@@ -22,10 +20,28 @@ before_filter :load_member_conditions
   def update
     @user = User.find(params[:id])
     
+    unless params[:has_role] == "" then
+      if @member_roles.include?(params[:has_role]) && @member_conditions.include?(params[:conditions]) then
+      
+        @role = @user.roles.new
+        @role.has_role = params[:has_role]
+        @role.conditions = params[:conditions]
+        @role.leader = false
+        
+        if @role.save then
+          #saved message
+        else
+          #failed message
+        end
+      
+      end # verify that this member can grant role.
+    
+    end     
+    
     respond_to do |format|
       if @user.update_attributes(params[:user])
         flash[:notice] = 'User was successfully updated.'
-        format.html { redirect_to( member_users_path) }
+        format.html { redirect_to( edit_member_user_path(params[:user])) }
       else
         flash[:notice] = 'Unable to save changes.'
         format.html { render :action => "edit" }
@@ -35,8 +51,12 @@ before_filter :load_member_conditions
 
   def destroy
     @user = User.find(params[:id])
+    @role = Role.find(params[:id])
+    @role.destroy
+    
     #currently will not delete users.
     #@user.destroy
+    
     respond_to do |format|
       format.html { redirect_to :back }
     end
