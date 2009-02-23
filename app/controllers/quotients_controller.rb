a48942923b314dbdfc1e5c32a8e41ccaa6fcd948
Phil <phil@phil-laptop.(none)>
2009-02-23 13:36:16 -0500

    quotient alow controller to handle nil

--- app/controllers/quotients_controller.rb
+++ app/controllers/quotients_controller.rb
@@ -42,69 +42,114 @@ class QuotientsController < ApplicationController
   def create
     @quotient = current_user.quotients.new(params[:quotient])
 
+ #raise @quotient.to_yaml
+
+  @quotient.f_score =   (@quotient.red_meat ||= 0) + 
+  (@quotient.fish ||= 0) + 
+  (@quotient.poultry ||= 0) +
+  (@quotient.vegetables_daily ||= 0) + 
+  (@quotient.fruit_daily ||= 0) +
+  (@quotient.vegetable_freshness ||= 0) + 
+  (@quotient.fruit_freshness ||= 0) + 
+  (@quotient.fried_foods ||= 0) + 
+  (@quotient.over_eat ||= 0) + 
+  (@quotient.food_variety ||= 0) + 
+  (@quotient.fast_eater ||= 0) + 
+  (@quotient.proper_potions ||= 0) + 
+  (@quotient.eat_frequently ||= 0) + 
+  (@quotient.daily_dessert ||= 0) + 
+  (@quotient.whole_grain_breads ||= 0 )
 
 
-    @quotient.f_score =  @quotient.red_meat + @quotient.fish + @quotient.poultry + @quotient.vegetables_daily + @quotient.fruit_daily + @quotient.vegetable_freshness + @quotient.fruit_freshness + @quotient.fried_foods + @quotient.over_eat + @quotient.food_variety + @quotient.fast_eater + @quotient.proper_potions + @quotient.eat_frequently + @quotient.daily_dessert + @quotient.whole_grain_breads 
+  #raise @quotient.to_yaml
 
-    @quotient.f_score /= 15  #get average based on number of questions.
 
-    @quotient.i_score = @quotient.happiness + 
-    
-    @quotient.smile_frequently +
-     @quotient.enjoy_daily_activities + 
-     @quotient.faith + 
-     @quotient.reached_potential +
-      @quotient.reach_goals_regularly + 
-      @quotient.have_inspiring_people + 
-      @quotient.listen_to_music + 
-      @quotient.have_hobby + 
-      @quotient.hours_tv_daily + @quotient.read_books + @quotient.daily_newspaper + @quotient.watch_sporting_events + @quotient.consider_news_negative + @quotient.talk_with_friends + @quotient.talk_with_family
-    
-    @quotient.i_score /= 15
-    
-    @quotient.b_score = @quotient.happy_with_body_type + @quotient.happy_with_mirror_reflection + @quotient.daily_exercise + @quotient.weekly_exercise + @quotient.able_to_walk_distance + @quotient.physical_limitations + @quotient.understand_fitness + @quotient.recent_operations + @quotient.recent_medical_tests + @quotient.chronic_medical_problems + @quotient.desired_life_span 
+  @quotient.f_score /= 15  #get average based on number of questions.
 
-@quotient.b_score /= 10 
- 
-    @quotient.e_score = @quotient.like_state_residence + @quotient.like_city_residence + 
-@quotient.like_geographical_area + @quotient.like_home + @quotient.like_working_environment + 
-@quotient.prefer_different_home_environment + @quotient.prefer_different_work_environment + 
-@quotient.planning_move + @quotient.planning_job_change + @quotient.planning_home_renovations  
+  @quotient.i_score = (@quotient.happiness||= 0) + 
+  (@quotient.smile_frequently||= 0) + 
+  (@quotient.enjoy_daily_activities||= 0) + 
+  (@quotient.faith ||= 0) + 
+  (@quotient.reached_potential ||= 0) + 
+  (@quotient.reach_goals_regularly ||= 0) + 
+  (@quotient.have_inspiring_people ||= 0) + 
+  (@quotient.listen_to_music ||= 0) + 
+  (@quotient.have_hobby ||= 0) + 
+  (@quotient.hours_tv_daily ||= 0) + 
+  (@quotient.read_books ||= 0) + 
+  (@quotient.daily_newspaper ||= 0) + 
+  (@quotient.watch_sporting_events ||= 0) + 
+  (@quotient.consider_news_negative ||= 0) + 
+  (@quotient.talk_with_friends ||= 0) + 
+  (@quotient.talk_with_family ||= 0) 
 
-    @quotient.e_score /= 10
+  @quotient.i_score /= 15
 
-    @quotient.r_score = @quotient.over_5_friends + @quotient.over_5_relatives + @quotient.have_to_many_friends + @quotient.have_to_many_relatives + @quotient.close_to_parents + 
-@quotient.close_to_aunts_uncles + @quotient.close_to_siblings + @quotient.close_to_inlaws
+  @quotient.b_score = (@quotient.happy_with_body_type ||= 0) + 
+  (@quotient.happy_with_mirror_reflection ||= 0) + 
+  (@quotient.daily_exercise ||= 0) + 
+  (@quotient.weekly_exercise ||= 0) + 
+  (@quotient.able_to_walk_distance ||= 0) + 
+  (@quotient.physical_limitations ||= 0) + 
+  (@quotient.understand_fitness ||= 0) + 
+  (@quotient.recent_operations ||= 0) + 
+  (@quotient.recent_medical_tests ||= 0) + 
+  (@quotient.chronic_medical_problems ||= 0) + 
+  (@quotient.desired_life_span ||= 0) 
+
+  @quotient.b_score /= 10 
+
+  @quotient.e_score = (@quotient.like_state_residence ||= 0) + 
+  (@quotient.like_city_residence ||= 0) + 
+  (@quotient.like_geographical_area ||= 0) + 
+  (@quotient.like_home ||= 0) + 
+  (@quotient.like_working_environment ||= 0) + 
+  (@quotient.prefer_different_home_environment ||= 0) + 
+  (@quotient.prefer_different_work_environment ||= 0) + 
+  (@quotient.planning_move ||= 0) + 
+  (@quotient.planning_job_change ||= 0) + 
+  (@quotient.planning_home_renovations  ||= 0) 
+
+  @quotient.e_score /= 10
+
+  @quotient.r_score = (@quotient.over_5_friends ||= 0) + 
+  (@quotient.over_5_relatives ||= 0) + 
+  (@quotient.have_to_many_friends ||= 0) + 
+  (@quotient.have_to_many_relatives ||= 0) + 
+  (@quotient.close_to_parents ||= 0) + 
+  (@quotient.close_to_aunts_uncles ||= 0) + 
+  (@quotient.close_to_siblings ||= 0) + 
+  (@quotient.close_to_inlaws ||= 0) 
 
   
-@quotient.r_score /= 9
+  @quotient.r_score /= 9
 
 
-@quotient.sleep_score = @quotient.sleep_enough + 
-@quotient.stay_up_late + 
-@quotient.wake_during_night + 
-@quotient.comfartable_while_sleeping + 
-@quotient.wake_rested + 
-@quotient.take_naps + 
-@quotient.have_dreams + 
-@quotient.remember_most_dreams + 
-@quotient.comfortable_bed + 
-@quotient.comfortable_pillow
+  @quotient.sleep_score = ( @quotient.sleep_enough ||= 0) + 
+  (@quotient.stay_up_late ||= 0) + 
+  (@quotient.wake_during_night ||= 0) + 
+  (@quotient.comfartable_while_sleeping ||= 0) + 
+  (@quotient.wake_rested ||= 0) + 
+  (@quotient.take_naps ||= 0) + 
+  (@quotient.have_dreams ||= 0) + 
+  (@quotient.remember_most_dreams ||= 0) + 
+  (@quotient.comfortable_bed ||= 0) + 
+  (@quotient.comfortable_pillow ||= 0 )
 
 
-@quotient.sleep_score /= 10
+  @quotient.sleep_score /= 10
 
 
-@quotient.stress_score = @quotient.currently_worried + 
-@quotient.worried_as_prior_year + 
-@quotient.worry_future + 
-@quotient.worry_finances + 
-@quotient.worry_about_health + 
-@quotient.witness_politics + 
-@quotient.count_todays_worries + 
-@quotient.easy_life 
+  @quotient.stress_score = (@quotient.currently_worried ||= 0) + 
+  (@quotient.worried_as_prior_year ||= 0) + 
+  (@quotient.worry_future ||= 0) + 
+  (@quotient.worry_finances ||= 0) + 
+  (@quotient.worry_about_health ||= 0) + 
+  (@quotient.witness_politics ||= 0) + 
+  (@quotient.count_todays_worries ||= 0) + 
+  (@quotient.easy_life ||= 0 )
 
-@quotient.stress_score /= 8
+  @quotient.stress_score /= 8
 
 
 
--- app/models/quotient.rb
+++ app/models/quotient.rb
@@ -1,3 +1,96 @@
 class Quotient < ActiveRecord::Base
 belongs_to :user
+
+def default 
+self.user = current_user
+self.red_meat  ||= 0
+self.fish  ||= 1
+self.poultry  ||= 1
+self.vegetables_daily  ||= 1
+self.fruit_daily  ||= 1
+self.vegetable_freshness  ||= 1
+self.fruit_freshness  ||= 1
+self.fried_foods  ||= 1
+self.over_eat  ||= 1
+self.food_variety  ||= 1
+self.fast_eater  ||= 1
+self.proper_potions  ||= 1
+self.eat_frequently  ||= 1
+self.daily_dessert  ||= 1
+self.whole_grain_breads  ||= 1
+
+self.happiness  ||= 1
+self.smile_frequently  ||= 1
+self.enjoy_daily_activities  ||= 1
+self.faith  ||= 1
+self.reached_potential  ||= 1
+self.reach_goals_regularly  ||= 1
+self.have_inspiring_people  ||= 1
+self.listen_to_music  ||= 1
+self.have_hobby  ||= 1
+self.hours_tv_daily  ||= 1
+self.read_books  ||= 1
+self.daily_newspaper  ||= 1
+self.watch_sporting_events  ||= 1
+self.consider_news_negative  ||= 1
+self.talk_with_friends  ||= 1
+self.talk_with_family  ||= 1
+
+self.happy_with_body_type  ||= 1
+self.happy_with_mirror_reflection  ||= 1
+self.daily_exercise  ||= 1
+self.weekly_exercise  ||= 1
+self.able_to_walk_distance  ||= 1
+self.physical_limitations  ||= 1
+self.understand_fitness  ||= 1
+self.recent_operations  ||= 1
+self.recent_medical_tests  ||= 1
+self.chronic_medical_problems  ||= 1
+self.desired_life_span  ||= 1
+
+self.like_state_residence  ||= 1
+self.like_city_residence  ||= 1
+self.like_geographical_area  ||= 1
+self.like_home  ||= 1
+self.like_working_environment  ||= 1
+self.prefer_different_home_environment  ||= 1
+self.prefer_different_work_environment  ||= 1
+self.planning_move  ||= 1
+self.planning_job_change  ||= 1
+self.planning_home_renovations  ||= 1
+
+self.over_5_friends  ||= 1
+self.over_5_relatives  ||= 1
+self.have_to_many_friends  ||= 1
+self.have_to_many_relatives  ||= 1
+self.close_to_parents  ||= 1
+self.close_to_aunts_uncles  ||= 1
+self.close_to_siblings  ||= 1
+self.close_to_inlaws  ||= 1
+
+
+self.sleep_enough  ||= 1
+self.stay_up_late  ||= 1
+self.wake_during_night  ||= 1
+self.comfartable_while_sleeping  ||= 1
+self.wake_rested  ||= 1
+self.take_naps  ||= 1
+self.have_dreams  ||= 1
+self.remember_most_dreams  ||= 1
+self.comfortable_bed  ||= 1
+self.comfortable_pillow  ||= 1
+
+self.currently_worried  ||= 1
+self.worried_as_prior_year  ||= 1
+self.worry_future  ||= 1
+self.worry_finances  ||= 1
+self.worry_about_health  ||= 1
+self.witness_politics  ||= 1
+self.count_todays_worries  ||= 1
+self.easy_life  ||= 1
+
+
+
+end # def initialize
+
 end
